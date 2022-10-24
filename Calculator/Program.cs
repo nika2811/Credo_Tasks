733486ddadb69717470aa175c7ad99ccc95090f9
Nick <nika.nacvlishvili1@gmail.com>
2022-10-24 22:00:44 +0400

    First Week

---
+++ Calculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool looping = false;
+            while (!looping) 
+            { 
+                Console.Write("\n\nEnter first number: ");
+                string numInput1 = Console.ReadLine();
+
+                double Num1 = 0;
+                while (!double.TryParse(numInput1, out Num1))
+                {
+                    Console.Write("Text is not a number, Enter first number: : ");
+                    numInput1 = Console.ReadLine();
+                }
+
+
+                Console.Write("Enter second number: ");
+                string numInput2 = Console.ReadLine();
+
+                double Num2 = 0;
+                while (!double.TryParse(numInput2, out Num2))
+                {
+                    Console.Write("Text is not a number, Enter second number: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+
+                Console.Write("Enter Math operation (+ - * /): ");
+                string oper = Convert.ToString(Console.ReadLine());
+
+
+                if(oper == "+")
+                {
+                    Console.Write(Num1 + " + " + Num2 + " = " + (Num1 + Num2));
+                }else if(oper == "-")
+                {
+                    Console.Write(Num1 + " - " + Num2 + " = " + (Num1 - Num2));
+                }else if(oper == "*")
+                {
+                    Console.Write(Num1 + " * " + Num2 + " = " + (Num1 * Num2));
+                }else if(oper == "/")
+                {
+                    if (Num2 == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                    }
+                    else
+                    {
+                        Console.Write(Num1 + " / " + Num2 + " = " + (Num1 / Num2));
+                    }
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ Nth Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Fibonacci
+{
+    public static void Main()
+    {
+
+        Console.Write("Input Number: ");
+        string numInput = Console.ReadLine();
+        double Num = 0;
+        double.TryParse(numInput, out Num);
+
+        int a = 0;
+        int b = 1;
+
+        if (Num == 0)
+        {
+            Console.WriteLine("0");
+        }
+        else if (Num == 1)
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            for (int i = 2; i < Num; i++)
+            {
+                b = a + b;
+                a = b - a;
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
