83a8097600419e91418d88327d0eee55653224a7
nika8 <nika.nacvlishvili1@gmail.com>
2022-10-31 00:44:52 +0400

    Vowel  or not

---
+++ Vowel or not/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static char target = 'e';
+
+        static void Main(string[] args)
+        {
+            char[] charArray = { 'a', 'e', 'i', 'o', 'u', 'ა', 'ე', 'ი', 'ო', 'უ' };
+
+            var check = Array.Exists(charArray, IsVowel);
+            if (check == true)
+            {
+                Console.WriteLine("{0} found in the array", target);
+            }
+            else
+            {
+                Console.WriteLine("Value not found");
+            }
+        }
+
+       
+        public static bool IsVowel(char x)
+        {
+            return x == target;
+        }
+    }
+}
