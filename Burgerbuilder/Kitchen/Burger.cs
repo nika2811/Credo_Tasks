8a8e5d77f766aa1d140ae48d309fce4d2b81693b
nika8 <nika.nacvlishvili1@gmail.com>
2022-11-15 00:44:48 +0400

    without yes or no

---
+++ Burgerbuilder/Kitchen/Burger.cs
@@ -0,0 +1,42 @@
+﻿using Burgerbuilder.Kitchen;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Burgerbuilder.Kitchen
+{
+    internal class Burger
+    {
+        public bool MeltedCheese { get; set; }
+        public bool Pickles { get; set; }
+        public string Meat { get; set; }
+        public string Sauce { get; set; }
+        public bool Lettuce { get; set; }
+        public bool Cookie { get; set; }
+
+        public Burger(bool meltedCheese, bool pickles, string meat, string sauce, bool lettuce, bool cookie)
+        {
+            MeltedCheese = meltedCheese;
+            Pickles = pickles;
+            Meat = meat;
+            Sauce = sauce;
+            Lettuce = lettuce;
+            Cookie = cookie;
+        }
+
+        public override string ToString()
+        {
+            return $"MeltedCheese: {MeltedCheese} \nPickles: {Pickles}\nMeat: {Meat}\nSauce: {Sauce}\nLettuce:{Lettuce}\nCookie:{Cookie}";
+        }
+
+
+    }
+
+}
+
+
---
+++ Burgerbuilder/Program.cs
@@ -0,0 +1,75 @@
+﻿using Burgerbuilder.Kitchen;
+
+
+
+var exit = false;
+while (!exit)
+{
+    PrintMenu();
+}
+
+void PrintMenu()
+{
+    Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+
+    Console.WriteLine("აირჩიეთ შეკვეთის რეჟიმი:");
+    Console.WriteLine("1. წინასწარ გამზადებული ბურგერები");
+    Console.WriteLine("2. ბურგერის აწყობა");
+
+
+    Console.Write("");
+    int.TryParse(Console.ReadLine(), out var BurgerKind);
+
+
+    var cheeseburger = new Burger(meltedCheese:true,pickles:true,meat:null,sauce: "Spicy", lettuce:true,cookie:true);
+    var hamburger = new Burger(meltedCheese: false, pickles: true, meat: "Beef", sauce: "Sweet", lettuce: true, cookie: true);
+    var branded = new Burger(meltedCheese: false, pickles: true, meat: "Chicken", sauce: "Branded", lettuce: true, cookie: true);
+
+
+    if (BurgerKind == 1)
+    {
+        Console.WriteLine("1. Cheeseburger");
+        Console.WriteLine("2. Hamburger");
+        Console.WriteLine("3. Branded");
+        Console.Write("");
+        int.TryParse(Console.ReadLine(), out var BurgerType);
+        if (BurgerType == 1)
+        {
+            Console.WriteLine($"{cheeseburger}");
+        }
+        else if (BurgerType == 2)
+        {
+            Console.WriteLine($"{hamburger}");
+        }
+        else if (BurgerType == 3)
+        {
+            Console.WriteLine($"{branded}");
+        }
+    }
+    else if (BurgerKind == 2)
+    {
+         bool MeltedCheese;
+         bool Pickles;
+         string Meat;
+         string Sauce;
+         bool Lettuce = true;
+         bool Cookie = true;
+        Console.Write("1. ჰქონდეს მდნარი ყველი?\n") ;
+        bool.TryParse(Console.ReadLine(), out MeltedCheese);
+        Console.Write("2. ჰქონდეს მჟავე კიტრი?\n");
+        bool.TryParse(Console.ReadLine(), out Pickles);
+        Console.Write("3. აირჩიეთ ხორცი:\n1.ქათმის\n2.საქონლის\n3.არცერთი\n");
+        Meat = Console.ReadLine();
+        Console.Write("3. აირჩიეთ სოუსი:\n1.ცხარე\n2.ტკბილი\n3.საფირმო\n4.არცერთი\n");
+        Sauce = Console.ReadLine();
+
+        Console.WriteLine($"MeltedCheese: {MeltedCheese} \nPickles: {Pickles}\nMeat: {Meat}\nSauce: {Sauce}\nLettuce:{Lettuce}\nCookie:{Cookie}");
+
+    }
+    
+
+
+
+}
