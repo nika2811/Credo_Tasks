5d5659c13f91beb5e6670ae8644fc94fbf96c1e6
nika8 <nika.nacvlishvili1@gmail.com>
2022-12-08 22:19:44 +0400

    Word Analyzer with multithreading

---
+++ WordAnalyzer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read all the words
+            string[] words = File.ReadAllText("words_alpha.txt")
+                                 .Split(new char[] { ' ', '\n', '\r' },
+                                         StringSplitOptions.RemoveEmptyEntries);
+
+            //Shortest and longest words 
+            string shortestWord = words.AsParallel()
+                                       .OrderBy(w => w.Length)
+                                       .First();
+            string longestWord = words.AsParallel()
+                                      .OrderBy(w => w.Length)
+                                      .Last();
+
+            Console.WriteLine($"Shortest word: {shortestWord}");
+            Console.WriteLine($"Longest word: {longestWord}");
+
+            // 100 words with the most vowels 
+            var wordsWithMostVowels = words.AsParallel()
+                                           .OrderByDescending(w => w.Count(c => "aeiou".Contains(c)))
+                                           .Take(100);
+
+            Console.WriteLine("100 words with the most vowels:");
+            foreach (var word in wordsWithMostVowels)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
