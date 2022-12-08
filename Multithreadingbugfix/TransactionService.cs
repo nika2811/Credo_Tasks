148e983646751c39f35f3f045a709f6b8f75c4a4
nika8 <nika.nacvlishvili1@gmail.com>
2022-12-09 00:34:08 +0400

    Multithreading bug fix

---
+++ Multithreadingbugfix/TransactionService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreadingbugfix
+{
+    internal class TransactionService
+    {
+
+    
+        private readonly object _lockObject = new object();
+        public void Transfer(BankAccount fromAccount, BankAccount toAccount, decimal amount)
+        {
+            lock (_lockObject)
+            {
+                if (amount > fromAccount.Balance)
+                {
+                    Console.WriteLine($"{fromAccount.Iban} has Insufficient funds");
+                    return;
+                }
+                fromAccount.Balance -= amount;
+                toAccount.Balance += amount;
+            }
+        }
+    }
+
+
+}
