16b21c617decfcbfbfbbb678da6966b1474b1c38
nika8 <nika.nacvlishvili1@gmail.com>
2023-01-06 19:34:59 +0400

    Move element to end

---
+++ MoveElementToEnd/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MoveElementToEnd;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var numArray = new List<int> { 2, 1, 2, 2, 2, 3, 4, 2 };
+        var move = 2;
+
+        void MoveToBottom(List<int> array, int toMove)
+        {
+            var result = array.Where(x => x != toMove).ToList();
+
+            result.AddRange(array.Where(x => x == toMove));
+
+            array.Clear();
+            array.AddRange(result);
+        }
+
+        MoveToBottom(numArray, move);
+        Console.WriteLine(string.Join(",", numArray));
+    }
+}
